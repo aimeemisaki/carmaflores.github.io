f0b9066debce60bf9a96040a36efa7ec26c87442
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2023-03-17 17:01:51 -0700

    imports images and updates FashionStyling.js styling

--- src/components/FashionStyling.js
+++ src/components/FashionStyling.js
@@ -1,5 +1,22 @@
 import React from 'react';
-import placeholderImg from '../assets/placeholderImg.png'
+import bolBasketballImg1 from '../assets/bolBasketballImg1.png';
+import bolBasketballImg2 from '../assets/bolBasketballImg2.jpg';
+import bolBasketballImg3 from '../assets/bolBasketballImg3.png';
+import popeyeRichImg from '../assets/popeyeRichImg.webp'
+import popeyeRichImg1 from '../assets/popeyeRichImg1.webp';
+import popeyeRichImg2 from '../assets/popeyeRichImg2.webp';
+import popeyeRichImg3 from '../assets/popeyeRichImg3.webp';
+import popeyeRichImg4 from '../assets/popeyeRichImg4.webp';
+import popeyeRichImg5 from '../assets/popeyeRichImg5.webp';
+import popeyeRichImg6 from '../assets/popeyeRichImg6.webp';
+import calebImg from '../assets/calebImg.png';
+import calebImg1 from '../assets/calebImg1.png';
+import calebImg2 from '../assets/calebImg2.png';
+import calebImg3 from '../assets/calebImg3.png';
+import calebImg4 from '../assets/calebImg4.png';
+import eartheaterImg from '../assets/eartheaterImg.png';
+import pufferImg from '../assets/pufferImg.png';
+
 
 function FashionStyling () {
     return (
@@ -13,25 +30,199 @@ function FashionStyling () {
                     Fashion Styling
                 </p>
                 </div>
-            <div className='flex flex-col'>
-                <img src={placeholderImg} alt='replace'/>
-                <p className='text-center text-lg' >Fashion Assistance</p>
+            <div className=''>
+                <img src={bolBasketballImg1} 
+                alt='Main Bol Basketball Styling'
+                className='main-bol-img'/>
+                
             </div>
             
             <div className='flex flex-col'>
-                    <p>
-                    CREATIVE ART DIRECTION / DIRTY SON AGENCY
-                    </p>
-                    <p>
-                    YEAR:
-                    </p>
-                    <p>
-                    CREDITS:
-                    </p>
+                <p className='text-lg' >Fashion Assistance</p>
+                <p>
+                CREATIVE ART DIRECTION / DIRTY SON AGENCY
+                </p>
+                <p>
+                YEAR:
+                </p>
+                <p>
+                CREDITS:
+                </p>
                 </div>
-            <div>
-
+        </div>
+        <div className=''>
+                <div className='alt-images flex flex-row overflow-x-auto'>
+                    <img 
+                    src={bolBasketballImg3} 
+                    alt="Second Bol Basketball Styling"
+                    className='alt-image bol-img-2' />
+                    <img 
+                    src={bolBasketballImg2}
+                    alt='Third Bol Basketball Styling'
+                    className='alt-image bol-img-3' />
+                </div>
+        </div>
+        <div className='grid grid-cols-3 gap-20 single-img'>
+            <div className='flex flex-col'>
+                <p className='text-right pl-8'>
+                    II.
+                </p>
+                <p className='text-right'>
+                    Fashion Styling
+                </p>
             </div>
+            <div className=''>
+                <img src={eartheaterImg} 
+                alt='Eartheater Styling'
+                className='eartheater-img'/>
+                
+            </div>
+            <div className='flex flex-col'>
+                <p className='text-lg' >Fashion Assistance</p>
+                <p>
+                CREATIVE ART DIRECTION / DIRTY SON AGENCY
+                </p>
+                <p>
+                YEAR: 2022
+                </p>
+                <p>
+                CREDITS:
+                </p>
+            </div>
+        </div>
+        <div className='grid grid-cols-3 gap-20 single-img'>
+            <div className='flex flex-col'>
+                <p className='text-right pl-8'>
+                    III.
+                </p>
+                <p className='text-right'>
+                    Fashion Styling
+                </p>
+            </div>
+            <div className=''>
+                <img 
+                src={pufferImg} 
+                alt='Puffer Styling'
+                className='puffer-img'/>
+                
+            </div>
+            <div className='flex flex-col'>
+                <p className='text-lg' >Fashion Assistance</p>
+                <p>
+                CREATIVE ART DIRECTION / DIRTY SON AGENCY
+                </p>
+                <p>
+                YEAR: 2022
+                </p>
+                <p>
+                CREDITS:
+                </p>
+            </div>
+        </div>
+        <div className='grid grid-cols-3 gap-20'>
+            <div className='flex flex-col'>
+                <p className='text-right pl-8'>
+                    IV.
+                </p>
+                <p className='text-right'>
+                    Fashion Styling
+                </p>
+            </div>
+            <div className=''>
+                <img src={popeyeRichImg} 
+                alt='Main Popeye Rich Styling'
+                className='popeye-main-img'/>
+                
+            </div>
+            <div className='flex flex-col'>
+                <p className='text-lg' >Fashion Assistance</p>
+                <p>
+                CREATIVE ART DIRECTION / DIRTY SON AGENCY
+                </p>
+                <p>
+                YEAR: 2022
+                </p>
+                <p>
+                CREDITS:
+                </p>
+            </div>
+        </div>
+        <div className=''>
+                <div className='overflow-images flex flex-row overflow-x-auto'>
+                    <img 
+                    src={popeyeRichImg1} 
+                    alt="Popeye Rich Styling" 
+                    className='alt-image popeye-img-1'/>
+                    <img 
+                    src={popeyeRichImg2} 
+                    alt="Popeye Rich Styling"
+                    className='alt-image popeye-img-2' />
+                    <img 
+                    src={popeyeRichImg3}
+                    alt='Popeye Rich Styling'
+                    className='alt-image popeye-img-3' />
+                    <img 
+                    src={popeyeRichImg4}
+                    alt='Popeye Rich Styling'
+                    className='alt-image popeye-img-4' />
+                    <img 
+                    src={popeyeRichImg5}
+                    alt='Popeye Rich Styling'
+                    className='alt-image popeye-img-5' />
+                    <img 
+                    src={popeyeRichImg6}
+                    alt='Popeye Rich Styling'
+                    className='alt-image popeye-img-6' />
+                </div>
+        </div>
+        <div className='grid grid-cols-3 gap-20'>
+            <div className='flex flex-col'>
+                <p className='text-right pl-8'>
+                    V.
+                </p>
+                <p className='text-right'>
+                    Fashion Styling
+                </p>
+            </div>
+            <div className=''>
+                <img 
+                src={calebImg} 
+                alt='Main Caleb Styling'
+                className='main-caleb-img'/>
+                
+            </div>
+            <div className='flex flex-col'>
+                <p className='text-lg' >Fashion Assistance</p>
+                <p>
+                CREATIVE ART DIRECTION / DIRTY SON AGENCY
+                </p>
+                <p>
+                YEAR:
+                </p>
+                <p>
+                CREDITS:
+                </p>
+            </div>
+        </div>
+        <div className=''>
+                <div className='alt-images flex flex-row overflow-x-auto'>
+                    <img 
+                    src={calebImg1} 
+                    alt="Caleb Styling"
+                    className='alt-image caleb-img-1' />
+                    <img 
+                    src={calebImg2}
+                    alt='Caleb Styling'
+                    className='alt-image caleb-img-2' />
+                    <img 
+                    src={calebImg3}
+                    alt='Caleb Styling'
+                    className='alt-image caleb-img-3' />
+                    <img 
+                    src={calebImg4}
+                    alt='Caleb Styling'
+                    className='alt-image caleb-img-4' />
+                </div>
         </div>
         </>
     )
