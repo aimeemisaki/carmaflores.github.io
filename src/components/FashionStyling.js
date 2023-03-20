da1c9d7c8c63d90cbe8466bf579c0c7cfc6488b4
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2023-03-20 01:10:55 -0700

    finalizes responsive design for FashionStyling.js

--- src/components/FashionStyling.js
+++ src/components/FashionStyling.js
@@ -21,206 +21,216 @@ import pufferImg from '../assets/pufferImg.png';
 function FashionStyling () {
     return (
         <>
-        <div className='work-container'>
-            <div className='flex flex-col cols-span-1'>
-                <p className='text-right pl-8'>
-                    I.
-                </p>
-                <p className='text-right text-title'>
-                    Fashion Styling
-                </p>
+        <div className='work-containers'>
+            <div className='work-container'>
+                <div className='flex flex-col cols-span-1'>
+                    <p className='text-right pl-8'>
+                        I.
+                    </p>
+                    <p className='text-right text-title'>
+                        Fashion Styling
+                    </p>
+                    </div>
+                <div className='main-img-container'>
+                    <img src={bolBasketballImg1} 
+                    alt='Main Bol Basketball Styling'
+                    className='main-bol-img main-img'/>
+                    
                 </div>
-            <div className='main-img-container'>
-                <img src={bolBasketballImg1} 
-                alt='Main Bol Basketball Styling'
-                className='main-bol-img main-img'/>
                 
+                <div className='text-section'>
+                    <p className='text-title'>Fashion Assistance</p>
+                    <p>
+                    CREATIVE ART DIRECTION / DIRTY SON AGENCY
+                    </p>
+                    <p>
+                    YEAR:
+                    </p>
+                    <p>
+                    CREDITS:
+                    </p>
+                    </div>
             </div>
-            
-            <div className='text-section'>
-                <p className='text-title'>Fashion Assistance</p>
-                <p>
-                CREATIVE ART DIRECTION / DIRTY SON AGENCY
-                </p>
-                <p>
-                YEAR:
-                </p>
-                <p>
-                CREDITS:
-                </p>
+            <div className=''>
+                    <div className='alt-images flex flex-row'>
+                        <img 
+                        src={bolBasketballImg3} 
+                        alt="Second Bol Basketball Styling"
+                        className='alt-image bol-img-2' />
+                        <img 
+                        src={bolBasketballImg2}
+                        alt='Third Bol Basketball Styling'
+                        className='alt-image bol-img-3' />
+                    </div>
+            </div>
+        </div>
+        <div className='work-containers'>
+            <div className='work-container'>
+                <div className='flex flex-col cols-span-1'>
+                    <p className='text-right pl-8'>
+                        II.
+                    </p>
+                    <p className='text-right text-title'>
+                        Fashion Styling
+                    </p>
                 </div>
-        </div>
-        <div className=''>
-                <div className='alt-images flex flex-row overflow-x-auto'>
-                    <img 
-                    src={bolBasketballImg3} 
-                    alt="Second Bol Basketball Styling"
-                    className='alt-image bol-img-2' />
-                    <img 
-                    src={bolBasketballImg2}
-                    alt='Third Bol Basketball Styling'
-                    className='alt-image bol-img-3' />
+                <div className='col-span-2 sm:col-span-1'>
+                    <img src={eartheaterImg} 
+                    alt='Eartheater Styling'
+                    className='eartheater-img main-img'/>
+                    
                 </div>
-        </div>
-        <div className='work-container'>
-            <div className='flex flex-col cols-span-1'>
-                <p className='text-right pl-8'>
-                    II.
-                </p>
-                <p className='text-right text-title'>
-                    Fashion Styling
-                </p>
-            </div>
-            <div className='col-span-2 sm:col-span-1'>
-                <img src={eartheaterImg} 
-                alt='Eartheater Styling'
-                className='eartheater-img main-img'/>
-                
-            </div>
-            <div className='text-section'>
-                <p className='text-sm sm:text-lg'>Fashion Assistance</p>
-                <p>
-                CREATIVE ART DIRECTION / DIRTY SON AGENCY
-                </p>
-                <p>
-                YEAR: 2022
-                </p>
-                <p>
-                CREDITS:
-                </p>
-            </div>
-        </div>
-        <div className='work-container'>
-            <div className='flex flex-col cols-span-1'>
-                <p className='text-right pl-8'>
-                    III.
-                </p>
-                <p className='text-right text-title'>
-                    Fashion Styling
-                </p>
-            </div>
-            <div className='main-img-container'>
-                <img 
-                src={pufferImg} 
-                alt='Puffer Styling'
-                className='puffer-img main-img'/>
-                
-            </div>
-            <div className='text-section'>
-                <p className='text-title' >Fashion Assistance</p>
-                <p>
-                CREATIVE ART DIRECTION / DIRTY SON AGENCY
-                </p>
-                <p>
-                YEAR: 2022
-                </p>
-                <p>
-                CREDITS:
-                </p>
-            </div>
-        </div>
-        <div className='work-container'>
-            <div className='flex flex-col cols-span-1'>
-                <p className='text-right pl-8'>
-                    IV.
-                </p>
-                <p className='text-right text-title'>
-                    Fashion Styling
-                </p>
-            </div>
-            <div className='main-img-container'>
-                <img src={popeyeRichImg} 
-                alt='Main Popeye Rich Styling'
-                className='popeye-main-img main-img'/>
-                
-            </div>
-            <div className='text-section'>
-                <p className='text-title' >Fashion Assistance</p>
-                <p>
-                CREATIVE ART DIRECTION / DIRTY SON AGENCY
-                </p>
-                <p>
-                YEAR: 2022
-                </p>
-                <p>
-                CREDITS:
-                </p>
-            </div>
-        </div>
-        <div className=''>
-                <div className='overflow-images flex flex-row overflow-x-auto'>
-                    <img 
-                    src={popeyeRichImg1} 
-                    alt="Popeye Rich Styling" 
-                    className='alt-image popeye-img-1'/>
-                    <img 
-                    src={popeyeRichImg2} 
-                    alt="Popeye Rich Styling"
-                    className='alt-image popeye-img-2' />
-                    <img 
-                    src={popeyeRichImg3}
-                    alt='Popeye Rich Styling'
-                    className='alt-image popeye-img-3' />
-                    <img 
-                    src={popeyeRichImg4}
-                    alt='Popeye Rich Styling'
-                    className='alt-image popeye-img-4' />
-                    <img 
-                    src={popeyeRichImg5}
-                    alt='Popeye Rich Styling'
-                    className='alt-image popeye-img-5' />
-                    <img 
-                    src={popeyeRichImg6}
-                    alt='Popeye Rich Styling'
-                    className='alt-image popeye-img-6' />
+                <div className='text-section'>
+                    <p className='text-sm sm:text-lg'>Fashion Assistance</p>
+                    <p>
+                    CREATIVE ART DIRECTION / DIRTY SON AGENCY
+                    </p>
+                    <p>
+                    YEAR: 2022
+                    </p>
+                    <p>
+                    CREDITS:
+                    </p>
                 </div>
-        </div>
-        <div className='work-container'>
-            <div className='flex flex-col cols-span-1'>
-                <p className='text-right pl-8'>
-                    V.
-                </p>
-                <p className='text-right text-title'>
-                    Fashion Styling
-                </p>
-            </div>
-            <div className='main-img-container'>
-                <img 
-                src={calebImg} 
-                alt='Main Caleb Styling'
-                className='main-caleb-img main-img'/>
-                
-            </div>
-            <div className='text-section'>
-                <p className='text-title' >Fashion Assistance</p>
-                <p>
-                CREATIVE ART DIRECTION / DIRTY SON AGENCY
-                </p>
-                <p>
-                YEAR:
-                </p>
-                <p>
-                CREDITS:
-                </p>
             </div>
         </div>
-            <div className='flex flex-row overflow-x-auto alt-images sm:overflow-images flex flex-row overflow-x-auto'>
-                <img 
-                src={calebImg1} 
-                alt="Caleb Styling"
-                className='alt-image caleb-img-1' />
-                <img 
-                src={calebImg2}
-                alt='Caleb Styling'
-                className='alt-image caleb-img-2' />
-                <img 
-                src={calebImg3}
-                alt='Caleb Styling'
-                className='alt-image caleb-img-3' />
-                <img 
-                src={calebImg4}
-                alt='Caleb Styling'
-                className='alt-image caleb-img-4' />
+        <div className='work-containers'>
+            <div className='work-container'>
+                <div className='flex flex-col cols-span-1'>
+                    <p className='text-right pl-8'>
+                        III.
+                    </p>
+                    <p className='text-right text-title'>
+                        Fashion Styling
+                    </p>
+                </div>
+                <div className='main-img-container'>
+                    <img 
+                    src={pufferImg} 
+                    alt='Puffer Styling'
+                    className='puffer-img main-img'/>
+                    
+                </div>
+                <div className='text-section'>
+                    <p className='text-title' >Fashion Assistance</p>
+                    <p>
+                    CREATIVE ART DIRECTION / DIRTY SON AGENCY
+                    </p>
+                    <p>
+                    YEAR: 2022
+                    </p>
+                    <p>
+                    CREDITS:
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div className='work-containers'>
+            <div className='work-container'>
+                <div className='flex flex-col cols-span-1'>
+                    <p className='text-right pl-8'>
+                        IV.
+                    </p>
+                    <p className='text-right text-title'>
+                        Fashion Styling
+                    </p>
+                </div>
+                <div className='main-img-container'>
+                    <img src={popeyeRichImg} 
+                    alt='Main Popeye Rich Styling'
+                    className='popeye-main-img main-img'/>
+                    
+                </div>
+                <div className='text-section'>
+                    <p className='text-title' >Fashion Assistance</p>
+                    <p>
+                    CREATIVE ART DIRECTION / DIRTY SON AGENCY
+                    </p>
+                    <p>
+                    YEAR: 2022
+                    </p>
+                    <p>
+                    CREDITS:
+                    </p>
+                </div>
+            </div>
+            <div className=''>
+                    <div className='overflow-images flex flex-row overflow-x-auto'>
+                        <img 
+                        src={popeyeRichImg1} 
+                        alt="Popeye Rich Styling" 
+                        className='alt-image popeye-img-1'/>
+                        <img 
+                        src={popeyeRichImg2} 
+                        alt="Popeye Rich Styling"
+                        className='alt-image popeye-img-2' />
+                        <img 
+                        src={popeyeRichImg3}
+                        alt='Popeye Rich Styling'
+                        className='alt-image popeye-img-3' />
+                        <img 
+                        src={popeyeRichImg4}
+                        alt='Popeye Rich Styling'
+                        className='alt-image popeye-img-4' />
+                        <img 
+                        src={popeyeRichImg5}
+                        alt='Popeye Rich Styling'
+                        className='alt-image popeye-img-5' />
+                        <img 
+                        src={popeyeRichImg6}
+                        alt='Popeye Rich Styling'
+                        className='alt-image popeye-img-6' />
+                    </div>
+            </div>
+        </div>
+        <div className='work-containers'>
+            <div className='work-container'>
+                <div className='flex flex-col cols-span-1'>
+                    <p className='text-right pl-8'>
+                        V.
+                    </p>
+                    <p className='text-right text-title'>
+                        Fashion Styling
+                    </p>
+                </div>
+                <div className='main-img-container'>
+                    <img 
+                    src={calebImg} 
+                    alt='Main Caleb Styling'
+                    className='main-caleb-img main-img'/>
+                    
+                </div>
+                <div className='text-section'>
+                    <p className='text-title' >Fashion Assistance</p>
+                    <p>
+                    CREATIVE ART DIRECTION / DIRTY SON AGENCY
+                    </p>
+                    <p>
+                    YEAR:
+                    </p>
+                    <p>
+                    CREDITS:
+                    </p>
+                </div>
+            </div>
+                <div className='flex flex-row caleb-images sm:alt-images '>
+                    <img 
+                    src={calebImg1} 
+                    alt="Caleb Styling"
+                    className='alt-image caleb-img-1' />
+                    <img 
+                    src={calebImg2}
+                    alt='Caleb Styling'
+                    className='alt-image caleb-img-2' />
+                    <img 
+                    src={calebImg3}
+                    alt='Caleb Styling'
+                    className='alt-image caleb-img-3' />
+                    <img 
+                    src={calebImg4}
+                    alt='Caleb Styling'
+                    className='alt-image caleb-img-4' />
+                </div>
             </div>
         </>
     )
