ae58f9a8dfeefb8b82712f962b21889fb944cd01
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2023-03-20 21:09:05 -0700

    builds AboutContact.js and builds route for AboutContact in App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LaunchPage from './components/LaunchPage';
 import NavBar from './components/NavBar';
 import FashionStyling from './components/FashionStyling';
 import Creative from './components/Creative';
+import AboutContact from './components/AboutContact';
 
 function App() {
   function videoStart () {
@@ -18,6 +19,7 @@ function App() {
             <Route path ='/' element={<LaunchPage />}/>
             <Route path ='/fashion-styling' element={<FashionStyling />} /> 
             <Route path ='/creative' element={<Creative/>} /> 
+            <Route path='/about-contact' element= {<AboutContact />} />
         </Routes>
       </main>
     </>
