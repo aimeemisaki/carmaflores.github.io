ca93670a448e3c56a20df78e0bec2273ced8d434
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2023-04-09 18:25:28 +0900

    updates logo video placement in LaunchPage.js

--- src/components/LaunchPage.js
+++ src/components/LaunchPage.js
@@ -4,7 +4,7 @@ import logoVideo from '../assets/logoVideo.mp4'
 function LaunchPage () {
     
     return (
-        <div className='centered sm:grid-centered'>
+        <div className='centered sm:flex-centered'>
             <video muted loop autoplay playsinline>
                 <source src={logoVideo} type='video/mp4' id='logo-video'>
                 </source>
